44ba8ea9df8287af75fa1bf7063c3c4bed9097b1
gcosta2107 <garragr10@gmail.com>
2025-05-26 03:49:15 -0300

    add product-validation-service logic

--- product-validation-service/src/main/java/br/com/microservices/orchestrated/productvalidationservice/core/producer/KafkaProducer.java
+++ product-validation-service/src/main/java/br/com/microservices/orchestrated/productvalidationservice/core/producer/KafkaProducer.java
@@ -13,14 +13,14 @@ public class KafkaProducer {
 
     private final KafkaTemplate<String, String> kafkaTemplate;
 
-    @Value("${spring.kafka.topic.orchestrator")
+    @Value("${spring.kafka.topic.orchestrator}")
     private String orchestratorTopic;
 
-    public void sendEvent(String payload){
-        try{
+    public void sendEvent(String payload) {
+        try {
             log.info("Sending event to topic {} with data {}", orchestratorTopic, payload);
             kafkaTemplate.send(orchestratorTopic, payload);
-        } catch (Exception ex){
+        } catch (Exception ex) {
             log.error("Error trying to send data to topic {} with data {}", orchestratorTopic, payload, ex);
         }
     }
